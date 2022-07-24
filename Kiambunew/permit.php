8a59beac12def60df0059040cfea1c2b128d4b83
Ann <93719602+wangui-ann@users.noreply.github.com>
2022-07-25 00:57:44 +0300

    Upload files

---
+++ Kiambunew/permit.php
@@ -0,0 +1,224 @@
+<?php 
+require_once 'core/init.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>parking - Parking</title>
+  <!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- CUSTOM STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+     <!-- GOOGLE FONTS-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+<?php
+     
+  $user = new User();
+  $users_id = escape($user->data()->users_id);
+  $surname = escape($user->data()->surname); 
+  $firstname = escape($user->data()->firstname);
+  $username = escape($user->data()->username);
+  $email = escape($user->data()->email);
+  $phone = escape($user->data()->phone);
+  $department = escape($user->data()->department);
+  $joined = escape($user->data()->joined);
+  $level = escape($user->data()->level);
+  $matric = escape($user->data()->matric);
+  $photo = escape($user->data()->photo);
+  $group = escape($user->data()->group);
+  $no  = "avatar.png";
+
+  if ($user->isLoggedIn()) {
+
+    if ($user->hasPermission('admin')) {
+       Redirect::to('admin.php');
+    }
+    elseif ($user->data()->group == '3') {
+       Redirect::to('lecturer.php');
+    }
+     
+?>
+<div id="wrapper">
+  <div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="adjust-nav">
+      <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          
+          <a class="navbar-brand" href="#">
+              <img src="assets/img/nacoss.jpg" style="width:60px; height:60px;" />
+          </a>
+      </div>
+
+        <span class="logout-spn">
+          <a href="logout.php" style="color:#fff; font-size:20px;">LOGOUT <i class="fa fa-sign-out"></i></a>  
+        </span>
+
+    </div>
+  </div>
+<!-- /. NAV TOP  -->
+      
+      <nav class="navbar-default navbar-side" role="navigation">
+      <div class="sidebar-collapse">
+      <ul class="nav" id="main-menu">
+          <li>
+            <a href="welcome.php?user=<?php echo $users_id; ?>" ><i class="fa fa-desktop"></i>Dashboard</a>
+          </li>
+          
+          <li class="active-link">
+            <a href="subappraise.php?user=<?php echo $users_id; ?>" ><i class="fa fa-home"></i>Rent</a>
+          </li>
+
+          <li class="active-link">
+            <a href="parking.php?user=<?php echo $users_id; ?>" ><i class="fa fa-car"></i>Parking</a>
+          </li>
+          
+          <li class="active-link">
+            <a href="permit.php?user=<?php echo $users_id; ?>" ><i class="fa fa-edit"></i>Business Permit</a>
+          </li>
+          
+
+      </ul>
+      </div>
+      </nav>
+        <!-- /. NAV SIDE  -->
+
+<div id="page-wrapper" >
+  <div id="page-inner">
+        
+        <div class="row text-center" style="margin-top:1%;">
+          <div class="col-md-6">
+            <?php $Rphoto = "<img src='profile/".$photo."' style='width:35px; height:35px;border-radius:50%;'>"; ?>
+            <h3 style=""><?php echo $Rphoto." ". $surname." ". $firstname; ?></h3>
+          </div>
+          <div class="col-md-6">
+            <h3><a href="welcome.php?user=<?php echo $users_id; ?>">Dashboard</a> / Parking Payment</h3>
+          </div>
+        </div>              
+       
+      <hr/>
+      
+      <?php if (Session::exists('home')) { ?>
+      <div class="row">
+        <div class="col-lg-12 ">
+          <div class="alert alert-info">
+            <i class="fa fa-volume-up"></i>
+               <?php echo Session::flash('home'); ?>
+          </div> 
+        </div>
+      </div>
+      <?php
+        }
+      ?>
+
+
+    <div class="row" style="margin-top:8%;">
+      <div class="col-md-1"></div>
+      <div class="col-md-10 jumbotron" style="padding:30px;border-radius:10px;">
+        <form method="POST" action="" enctype="multipart/form-data">
+          <?php require 'processparking.php'; ?>
+        <div class="form-group">
+          <label>Enter Vehicle No <span style="color:red;">*</span></label>
+          <input class="form-control" type='text' style="padding:18px;" name="ehicleNo" placeholder="Enter vehicle no">
+        </div>
+        <div class="form-group">
+          <label> Enter Vehicle Type <span style="color:red;">*</span></label>
+          <input class="form-control" type='text' style="padding:18px;" name="ehicleType" placeholder="Enter vehicle type">
+        </div>
+        <div class="form-group" style="margin-top:2%;">
+          <label>Payment Month <span style="color:red;">*</span></label>
+          <select class="form-control" name="Month">
+            <option value="No Month"> Select Month </option>
+            <option value="January">January</option>
+            <option value="February">February</option>
+            <option value="March">March</option>
+            <option value="April">April</option>
+            <option value="May">May</option>
+            <option value="June">June</option>
+            <option value="July">July</option>
+            <option value="August">August</option>
+            <option value="September">September</option>
+            <option value="October">October</option>
+            <option value="November">November</option>
+            <option value="December">December</option>
+          </select>
+        </div>
+        
+        <div class="form-group">
+          <label>Amount <span style="color:red;">*</span></label>
+          <input class="form-control" type='text' style="padding:18px;" name="Amount" placeholder="enter amount paid">
+        </div>
+        <div class="row text-center" style="margin-top:6%;">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+          <div class="form-group">
+            <input type="hidden" name="token" value="<?php echo Token::generate();?>">
+            <button class="btn btn-primary btn-lg"><i class="fa fa-send-o"></i> Submit</button>
+          </div>
+        </div>
+        <div class="col-md-4"></div>
+        </div>
+
+
+        </form>
+      </div>
+      <div class="col-md-1"></div>
+    </div>
+
+
+  </div> <!-- /. PAGE INNER  -->
+</div><!-- /. PAGE WRAPPER  -->
+
+</div><!-- /. PAGE WRAPPER 2  -->
+
+
+
+
+
+<!-- FOOTER HERE -->
+
+<div class="footer">
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            &copy; 2020 All Rights Reserved Kiambu County Council Portal
+        </div>
+    </div>
+</div>
+          
+<?php
+
+}
+
+else
+
+{
+
+  redirect::to('login.php');
+
+}
+
+?>
+
+
+<!-- /. WRAPPER  -->
+<!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+<!-- JQUERY SCRIPTS -->
+<script src="assets/js/jquery-1.10.2.js"></script>
+<!-- BOOTSTRAP SCRIPTS -->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- CUSTOM SCRIPTS -->
+<script src="assets/js/custom.js"></script>
+    
+   
+</body>
+</html>
